d32124ca8e3faa91679c6b680152e19c06d3d638
Yongzhi Xu <785931268@qq.com>
2021-07-07 12:18:32 +0800

    add test unit

--- cs3303datastructures/src/test/java/edu/uopeople/cs3303datastructures/Cs3303datastructuresApplicationTests.java
+++ cs3303datastructures/src/test/java/edu/uopeople/cs3303datastructures/Cs3303datastructuresApplicationTests.java
@@ -1,13 +1,16 @@
 package edu.uopeople.cs3303datastructures;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class Cs3303datastructuresApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
---
+++ cs3303datastructures/src/test/java/edu/uopeople/cs3303datastructures/unit2/AlgotithmCompareTest.java
@@ -0,0 +1,33 @@
+package edu.uopeople.cs3303datastructures.unit2;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+/**
+ * AlgotithmCompare Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>07/07/2021</pre>
+ */
+public class AlgotithmCompareTest {
+
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    /**
+     * Method: main(String[] args)
+     */
+    @Test
+    public void testMain() throws Exception {
+//TODO: Test goes here... 
+    }
+
+
+} 
